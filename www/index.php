0c60c166adef42065bbc8e5eb827a64e4fb63096
Muki89 <teemu.siltanen23@gmail.com>
2017-01-25 11:58:26 +0200

    eka commit

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FC Otsot</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+<?php 
+	include('app/views/header.php');
+	include('app/views/content.php'); 
+	include('app/views/footer.php');
+?>
+</body>
+</html>
